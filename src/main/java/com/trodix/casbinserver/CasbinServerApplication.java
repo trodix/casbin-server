60f8ac97021cbaaffea935f62ce4d55156fb0262
Sebastien Vallet <sebastien.vallet89@gmail.com>
2023-10-14 14:22:35 +0200

    initial commit

---
+++ src/main/java/com/trodix/casbinserver/CasbinServerApplication.java
@@ -0,0 +1,13 @@
+package com.trodix.casbinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasbinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasbinServerApplication.class, args);
+	}
+
+}
